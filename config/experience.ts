8cc5912ecbb406956a57642c88b366a0d8e34672
Calvin <calvinng1993@gmail.com>
2024-02-23 10:24:06 +0000

    Finish experience config

--- config/experience.ts
+++ config/experience.ts
@@ -5,11 +5,15 @@ import { FaSuitcase } from "react-icons/fa";
 
 export const experienceConfig = [
   {
-    title: "Graduated from Bootcamp",
+    title: "Graduate Software Engineer",
     company: "BrainStation",
     location: "London, UK",
-    description:
+    description: [
       "I graduated from BrainStation bootcamp after 3 months of learning. I am now looking for opportunities as a front-end developer.",
+      "Learning industry best practices throughout the course to become a proficient software developer.",
+      "Developing foundational skills in web development, progressing from basic multi-page web design to advanced Single Page Application development using React.",
+      "Back-end development with technologies such as Node.js and Express, effectively setting up frameworks and utilising MySQL databases for seamless data management and persistence.",
+    ],
     icon: React.createElement(FaGraduationCap),
     date: "Nov 2023 - Feb 2024",
   },
@@ -17,8 +21,10 @@ export const experienceConfig = [
     title: "Senior Antenna Engineer",
     company: "Leonardo UK",
     location: "Luton, UK",
-    description:
-      "I graduated from BrainStation bootcamp after 3 months of learning. I am now looking for opportunities as a front-end developer.",
+    description: [
+      "Design and develop antennas & radomes for function and RCS",
+      "Nominated and Received an award for Technology and Innovation",
+    ],
     icon: React.createElement(FaSuitcase),
     date: "Aug 2023 - Nov 2023",
   },
@@ -26,8 +32,11 @@ export const experienceConfig = [
     title: "Antenna and Microwave Engineer",
     company: "Meta Material Inc.",
     location: "London, UK",
-    description:
-      "I graduated from BrainStation bootcamp after 3 months of learning. I am now looking for opportunities as a front-end developer.",
+    description: [
+      "Conference paper accepted IEEE APS 2023",
+      "Lead and manage various antenna design projects",
+      "Combine RF technologies with metamaterial for R&D",
+    ],
     icon: React.createElement(FaSuitcase),
     date: "Jun 2022 - Jul 2023",
   },
@@ -35,8 +44,11 @@ export const experienceConfig = [
     title: "Senior Antenna RF Design Engineer",
     company: "Airbus",
     location: "Stevenage, UK",
-    description:
-      "I graduated from BrainStation bootcamp after 3 months of learning. I am now looking for opportunities as a front-end developer.",
+    description: [
+      "Accountable for the transmit antenna on a satellite platform",
+      "Developed algorithms to improve productivity across the team.",
+      "Participate in and manage proposal activities",
+    ],
     icon: React.createElement(FaSuitcase),
     date: "Sept 2018 - Jun 2022",
   },
@@ -44,7 +56,10 @@ export const experienceConfig = [
     title: "RF Design Engineer",
     company: "Omni-ID",
     location: "Farnham, UK",
-    description: ["", ""],
+    description: [
+      "Understanding RFID both in Hardware and Software.",
+      "Producing a commercially ready RFID Tag used for plant trolleys.",
+    ],
     icon: React.createElement(FaSuitcase),
     date: "Apr 2018 - Sept 2018",
   },
