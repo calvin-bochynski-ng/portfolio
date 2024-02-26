189bb5e72c6fd4ce8fea0a549612946d1c35bc38
Calvin <calvinng1993@gmail.com>
2024-02-26 16:51:41 +0000

    Add domain name to allow Origins

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const nextConfig = {
         "localhost:3000",
         "192.168.1.120:3000",
         "calvin-bochynski-ng-portfolio.netlify.app",
+        "calvin.bochynski-ng.com",
       ],
     },
   },
