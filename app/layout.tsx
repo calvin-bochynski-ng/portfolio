1aab3629835983a8dd654ec2bea8ae6767400e72
Calvin <calvinng1993@gmail.com>
2024-02-28 10:00:36 +0000

    Change particle background to before navbar and children to avoid overlay

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,6 +54,7 @@ export default function RootLayout({
         <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
           <ActiveSectionContextProvider>
             <Toaster />
+            <ParticlesBackground />
             <div className="relative flex flex-col">
               <Navbar />
               <main className="container mx-auto max-w-7xl px-6 flex flex-col">
@@ -61,7 +62,6 @@ export default function RootLayout({
               </main>
             </div>
             {/* <Footer /> */}
-            <ParticlesBackground />
           </ActiveSectionContextProvider>
         </Providers>
       </body>
