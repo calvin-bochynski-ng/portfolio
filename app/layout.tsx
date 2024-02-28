99edd482a75d54b63ce6fd5465f5f8e293edd9fe
Calvin <calvinng1993@gmail.com>
2024-02-28 09:56:13 +0000

    Change particle background to the end of the page loading to avoid blocking

--- app/layout.tsx
+++ app/layout.tsx
@@ -51,10 +51,9 @@ export default function RootLayout({
           fontSans.variable
         )}
       >
-        <Toaster />
-        <ParticlesBackground />
         <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
           <ActiveSectionContextProvider>
+            <Toaster />
             <div className="relative flex flex-col">
               <Navbar />
               <main className="container mx-auto max-w-7xl px-6 flex flex-col">
@@ -62,6 +61,7 @@ export default function RootLayout({
               </main>
             </div>
             {/* <Footer /> */}
+            <ParticlesBackground />
           </ActiveSectionContextProvider>
         </Providers>
       </body>
